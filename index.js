e8df3ae0561f6b2146c7cd8f9637f3125faa26cc
forhadhossain60 <forhadhossain60@outlook.com>
2023-05-22 08:02:54 +0600

    base

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = ViheoPlayer;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function ViheoPlayer() {
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    className: "viheo"
+  });
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ViheoPlayer(){
+  return (
+    <div className='viheo'>
+      
+    </div>
+  )
+}
